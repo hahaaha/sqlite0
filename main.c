7d706f0fee607027568d9a240bbb97975f557f34
haha <390519764@qq.com>
2023-11-29 17:17:13 +0800

    feature: 增加光标

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
@@ -40,6 +41,12 @@ typedef struct {
   uint32_t num_rows;
   Pager* pager;
 } Table;
+
+typedef struct {
+  Table* table;
+  uint32_t row_num;
+  bool end_of_table;
+} Cursor;
 
 typedef struct {
   char* buffer;
@@ -170,11 +177,30 @@ PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
   return PREPARE_SUCCESS;
 }
 
+Cursor* table_start(Table* table) {
+  Cursor* cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->row_num = 0;
+  cursor->end_of_table = (table->num_rows == 0);
+  
+  return cursor;
+}
+
+Cursor* table_end(Table* table) {
+  Cursor* cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->row_num = table->num_rows;
+  cursor->end_of_table = true;
+
+  return cursor;
+}
+
 ExecuteResult execute_insert(Statement* statement, Table* table) {
   if(table->num_rows >= TABLE_MAX_ROWS) {
     return EXECUTE_TABLE_FULL;
   }
   Row* row_to_insert = &(statement->row_to_insert);
+  Cursor* cursor = table_end(table);
 
   serialize_row(row_to_insert, row_slot(table, table->num_rows));
   table->num_rows +=1;
@@ -417,15 +443,21 @@ void db_close(Table* table) {
   free(table);
 }
 
-void* row_slot(Table* table, uint32_t row_num ) {
+void* cursor_value(Cursor* cursor) {
+  uint32_t row_num = cursor->row_num;
   uint32_t page_num = row_num / ROWS_PRE_PAGE;
   // void* page = table->pages[page_num];
   //if(page == NULL) {
   //  page = table->pages[page_num] = malloc(PAGE_SIZE);
   //  }
-  void* page = get_page(table->pager, page_num);
+  void* page = get_page(cursor->table->pager, page_num);
   uint32_t row_offset = row_num % ROWS_PRE_PAGE;
   uint32_t byte_offset = row_offset * ROW_SIZE;
   return page + byte_offset;
+}
+
+void* cursor_advance(Cursor* cursor) {
+  cursor->row_num += 1;
+  if(cursor->row_num )
 }
 
